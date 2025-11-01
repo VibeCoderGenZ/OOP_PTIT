8678a11879b1fa19270c7400b547c83492bebfeb
Mai Huu Son <maihuuson2005@gmail.com>
2025-11-01 23:19:25 +0700

    ac

--- J01002.java
+++ J01002.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class J01002 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            System.out.println(a * (a + 1) / 2);
+        }
+        sc.close();
     }
 }
