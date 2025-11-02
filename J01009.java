971387a755438b23a38256ee7f88778fd52e0d71
Mai Huu Son <maihuuson2005@gmail.com>
2025-11-03 00:36:50 +0700

    ac

--- J01009.java
+++ J01009.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class J01010 {
+public class J01009 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
